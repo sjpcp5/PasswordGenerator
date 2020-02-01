b523b548dea50450cc631c1a7e5ae7a8284145c0
Cutie on the Couch <sjpcp5@gmail.com>
2020-02-01 15:46:27 -0600

    troubleshooting why I am gettin NaN in the password generator

--- script.js
+++ script.js
@@ -11,32 +11,157 @@
 //variables
 var generateBtn = document.querySelector("#generate");
 var passwordEl = document.querySelector("#password");
+
+function checkUserResponse(){
+
+var promptPassLength = parseInt(prompt("How long do you want the password to be? "));
+console.log(promptUppercase);
+console.log(promptPassLength);
+
+
+if (isNan(promptPassLength)===true){
+  alert("Password length must be provided as a number");
+  return;
+}
+
+if (promptPassLength < 8) {
+  alert("Password length must be at least 8 characters");
+  return;
+}
+if (promptPassLength > 128) {
+  alert("Password length must less than 129 characters");
+  return;
+}
 var promptUppercase = confirm("Do you want uppercase characters? ");
 var promptLowercase = confirm("Do you want lowercase characters? ");
 var promptSpecialcharac = confirm("Do you want special characters? ");
 var promptNumbers = confirm("Do you want numbers characters? ");
-var promptPassLength = prompt("How long do you want the password to be? ");
 
+if (
+  promptLowercase === false &&
+  promptUppercase === false &&
+  promptNumbers === false &&
+  promptSpecialcharac === false
+) {
+  alert("Must select at least one character type");
+  return;
+};
+return; 
+};
 
 //console log for enitre if funtions and confirms working together
 // console.log("The users password length is..", + promptPassLength + " The user chose " + " UC ", + promptUppercase + " LC ", + promptLowercase + " Num ", + promptNumbers + " Special ", + promptSpecialcharac,);
 
 //generator functions - http://www.net-comber.com/charset.html
-
-function getRandomUpper () {
-  return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+var upperCasedCharacters = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z"
+];
+var lowerCasedCharacters = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z"
+];
+var numericCharacters = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var specialCharacters = [
+  "@",
+  "%",
+  "+",
+  "\\",
+  "/",
+  "'",
+  "!",
+  "#",
+  "$",
+  "^",
+  "?",
+  ":",
+  ",",
+  ")",
+  "(",
+  "}",
+  "{",
+  "]",
+  "[",
+  "~",
+  "-",
+  "_",
+  "."
+];
+function getRandomUpper() {
+  let randomIndex = Math.floor(Math.random() * 26);
+  let randomUpper = upperCasedCharacters[randomIndex];
+  console.log(randomUpper);
+  return randomUpper;
 };
 
-function getRandomLower () {
-  return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+function getRandomLower() {
+  let randomIndex = Math.floor(Math.random() * 26);
+  let randomLower = lowerCasedCharacters[randomIndex];
+  console.log(randomLower);
+  return randomLower;
 };
 
-function getRandomNumber () {
-  return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+function getRandomNumber() {
+  let randomIndex = (Math.floor(Math.random() * 10));
+  let randomNumber = numericCharacters[randomIndex];
+  console.log(randomNumber);
+  return randomNumber;
 };
 
-function getSpecialCharc () {
-  return String.fromCharCode(Math.floor(Math.random() * 15) + 33);
+function getSpecialCharc() {
+  let randomIndex = (Math.floor(Math.random() * 24));
+  let randomSpecialChar = specialCharacters[randomIndex];
+  console.log(randomSpecialChar);
+
+  return randomSpecialChar;
 };
 
 // console.log(getRandomLower(), getRandomNumber(), getSpecialCharc(), getRandomUpper());
@@ -45,45 +170,55 @@ function getSpecialCharc () {
 };*/
 
 
+
 //functions to generate user's password
 // for loop and array that gathers all confirmed funcitons to create password
 //utilize arrays and math.random to generate a password
-function generatePass () {
+function generatePass() {
+  var options = checkUserResponse(); 
   var passwordString = "";
+  console.log(passwordString);
   //passwordEl = localStorage.getItem("passwordString")
   var possibleChars = [];
-
+  var guaranteedCharacters = [];
   // if they want upper case add one to possibleChars
   if (promptUppercase) {
+    possibleChars = possibleChars.concat(upperCasedCharacters);
     let randomUpper = getRandomUpper();
-    possibleChars.push(randomUpper);
+    guaranteedCharacters.push(randomUpper);
+    console.log(possibleChars);
   }
 
 
   // if they want lower case ...
   if (promptLowercase) {
+    possibleChars = possibleChars.concat(lowerCasedCharacters);
     let randomLower = getRandomLower();
-    possibleChars.push(randomLower);
+    guaranteedCharacters.push(randomLower);
   }
 
 
   // if they want symbols ....
   if (promptSpecialcharac) {
+    possibleChars = possibleChars.concat(specialCharacters);
     let randomChar = getSpecialCharc();
-    possibleChars.push(randomChar);
+    guaranteedCharacters.push(randomChar);
   }
 
   // if they want num ...
   if (promptNumbers) {
+    possibleChars = possibleChars.concat(numericCharacters);
     let randomNumber = getRandomNumber();
-    possibleChars.push(randomNumber);
+    guaranteedCharacters.push(randomNumber);
+    console.log(possibleChars);
+    console.log(guaranteedCharacters)
   }
 
   for (var i = 0; i < promptPassLength; i++) {
-    
-
-    let randomIndex = Math.floor(Math.random() * possibleChars.length)
-    passwordString += possibleChars[randomIndex];
+    /* let randomIndex = Math.floor(Math.random() * possibleChars.length)
+    var passwordString = + possibleChars[randomIndex]; */
+    console.log(promptPassLength);
+    console.log(passwordString);
   };
 
   passwordEl.innerText = passwordString;
